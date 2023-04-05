82ca336a8b5d463bfc07ac2389f875fbb041aa5b
Luis Lucas <you@example.com>
2023-04-04 23:58:25 -0300

    Up abr 4 23:58

--- src/countBy.ts
+++ src/countBy.ts
@@ -5,8 +5,8 @@ export interface CountList {
 export default function countBy(arr: (string | number)[]) {
   return arr.reduce((acc: CountList, item) => {
     if (acc[item]) {
-      acc[item] +=1;
-    }else {
+      acc[item] += 1;
+    } else {
       acc[item] = 1;
     }
     return acc;
